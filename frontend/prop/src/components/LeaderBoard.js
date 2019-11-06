759bbf93d421e891d0dc770639a65d4f93452505
Amit Shah <amitshah0t7@gmail.com>
2019-11-06 13:31:08 -0500

    add TTF font loader so we dont have to use canvas texture possibly for special fonts

--- frontend/prop/src/components/LeaderBoard.js
+++ frontend/prop/src/components/LeaderBoard.js
@@ -1,11 +1,18 @@
 import React, {useRef} from 'react';
 import {Button,Card}  from 'react-bootstrap';
 import * as THREE from "three";
+import TTFLoader from '../TTFLoader';
+
 
 class LeaderBoard extends React.Component {
 
   componentDidMount(){
-     var loader = new THREE.CanvasTexture();
+     var canvasTexture = new THREE.CanvasTexture();
+     const loader = new TTFLoader();
+     const fontLoader = new THREE.FontLoader();
+     var font = null;
+     loader.load('/assets/fonts/fontawesome-webfont.ttf',fnt => font = fontLoader.parse(fnt))
+     
   }
 
 
@@ -22,7 +29,14 @@ class LeaderBoard extends React.Component {
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
+       <div
+        style={{ width: "800px", height: "800px" }}
+        ref={mount => {
+          this.mount = mount;
+        }}
+      />
     </Card>
+   
     );
   
    }
